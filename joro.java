3a020f3829d539a2d94020aa7bcc04e9c93f0fc3
Anurag-Bharde <anwolfragspitter99@gmail.com>
2023-11-21 20:02:32 +0530

    hiphen

---
+++ joro.java
@@ -0,0 +1,20 @@
+
+class trk {
+
+    String name;
+    int age;
+    trk(String name,int age) {
+
+    }
+    trk(trk pm) {
+        System.out.println("gfdd");
+    }
+}
+
+
+public class joro {
+    public static void main(String[] args) {
+        trk dl = new trk("name", 4);
+    }
+
+}
